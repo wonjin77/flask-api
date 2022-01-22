993d94d1e701633b61c7fab2666ce57edd04bfb4
wonjin77 <wwonginko@gmail.com>
2022-01-23 07:19:39 +0900

    modified

--- flask_ui/app_ui.py
+++ flask_ui/app_ui.py
@@ -12,29 +12,29 @@ def index():
 
 @app.route('/Movies')
 def total():
-    # address = 'http://flask-rest-api-service:5001/Movies'
-    address = 'http://localhost:5001/Movies/'
+    address = 'http://flask-rest-api-service:5001/Movies'
+    # address = 'http://localhost:5001/Movies/'
     r = requests.get(address) 
     total_movie = eval(r.text)
     df_info = pd.DataFrame(data=total_movie['movie_info']).T
     df_info.to_html('templates/df_html.html', classes='movie_info')
     return render_template('df_html.html')
 
-@app.route('/Movies/upload')
-def create(title_id, title, genre):
-    # address = 'http://flask-rest-api-service:5001/Movies'
-    address = f'http://localhost:5001/Movies/{title_id}'
-    data = {
-        'title': title,
-        'genre': genre,
-    }
-    r = requests.post(address, data=data)
-    return r.content
+# @app.route('/Movies/upload')
+# def create(title_id, title, genre):
+#     address = f'http://flask-rest-api-service:5001/Movies/{title_id}'
+#     # address = f'http://localhost:5001/Movies/{title_id}'
+#     data = {
+#         'title': title,
+#         'genre': genre,
+#     }
+#     r = requests.post(address, data=data)
+#     return r.content
 
 @app.route('/Raw')
 def get_raw():
-    # address = 'http://flask-rest-api-service:5001/Movies'
-    address = 'http://localhost:5001/Movies/'
+    address = 'http://flask-rest-api-service:5001/Movies'
+    # address = 'http://localhost:5001/Movies/'
     r = requests.get(address) 
     total_movie = eval(r.text)
     return total_movie['movie_info']
