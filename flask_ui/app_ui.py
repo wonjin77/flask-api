0ff304e1eb35d2439af659cfdae3f32cf0cba828
wonjin77 <wwonginko@gmail.com>
2022-01-23 15:35:50 +0900

    modified

--- flask_ui/app_ui.py
+++ flask_ui/app_ui.py
@@ -5,6 +5,9 @@ import pandas as pd
 import html
 
 app = Flask(__name__)
+address = 'http://flask-rest-api-service.flask.svc.kr-central-1.c.kakaoi.io:5001/Movies/'
+# address = 'http://localhost:5001/Movies/'
+
 
 @app.route("/")
 def index():
@@ -12,32 +15,17 @@ def index():
 
 @app.route('/Movies')
 def total():
-    address = 'http://flask-rest-api-service:5001/Movies'
-    # address = 'http://localhost:5001/Movies/'
     r = requests.get(address) 
     total_movie = eval(r.text)
     df_info = pd.DataFrame(data=total_movie['movie_info']).T
     df_info.to_html('templates/df_html.html', classes='movie_info')
     return render_template('df_html.html')
 
-# @app.route('/Movies/upload')
-# def create(title_id, title, genre):
-#     address = f'http://flask-rest-api-service:5001/Movies/{title_id}'
-#     # address = f'http://localhost:5001/Movies/{title_id}'
-#     data = {
-#         'title': title,
-#         'genre': genre,
-#     }
-#     r = requests.post(address, data=data)
-#     return r.content
-
 @app.route('/Raw')
 def get_raw():
-    address = 'http://flask-rest-api-service:5001/Movies'
-    # address = 'http://localhost:5001/Movies/'
     r = requests.get(address) 
     total_movie = eval(r.text)
     return total_movie['movie_info']
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(port=5000, debug=True)
